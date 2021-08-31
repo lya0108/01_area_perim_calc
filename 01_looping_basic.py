1cb661f430d890d6b43127e82eeec3f065d04761
lya0108 <lya0108@masseyhigh.school.nz>
2021-08-31 14:02:22 +1200

    Update 01_looping_basic.py

--- 01_looping_basic.py
+++ 01_looping_basic.py
@@ -1,6 +1,12 @@
 valid = False
 while not valid:
     print("this is a loop")
+
     response = float(input("enter a number: "))
+
     if response > 0:
         valid = True
+
+    else:
+        print("please enter a number that is more than zero")
+        print() 
