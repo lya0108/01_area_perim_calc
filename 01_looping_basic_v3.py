4a7cecdebdfa044806676ddc4fcc947ddf1d282f
lya0108 <lya0108@masseyhigh.school.nz>
2021-08-31 14:02:13 +1200

    Create 01_looping_basic_v3.py

---
+++ 01_looping_basic_v3.py
@@ -0,0 +1,34 @@
+
+
+# checks input is a number more than zero
+def num_check(question):
+
+
+    valid = False
+    while not valid:
+
+        error = "please enter a number that is more than zero"
+
+        try:
+
+            response = float(input(question))
+
+            if response > 0:
+                return response 
+
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+            print()
+
+
+
+width = num_check("width: ")
+height = num_check("height: ")
+print()
+print("width", width)
+print("height", height)
+print()
